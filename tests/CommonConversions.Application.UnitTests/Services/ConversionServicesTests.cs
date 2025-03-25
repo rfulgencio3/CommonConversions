5a83bb88389adbd69cbe43433480ee4a1db1e5d8
Ricardo Fulgencio Alves <ricardo.fulgencio@hotmail.com>
2025-03-25 18:47:46 -0300

    test: Criação de projetos de teste

---
+++ tests/CommonConversions.Application.UnitTests/Services/ConversionServicesTests.cs
@@ -0,0 +1,46 @@
+﻿using CommonConversions.Application.Services;
+
+namespace CommonConversions.Application.UnitTests.Services;
+
+public class ConversionServiceTests
+{
+    [Fact]
+    public void CelsiusToFahrenheit_ShouldConvertCorrectly()
+    {
+        // Arrange
+        var service = new ConversionService();
+        double input = 0;
+
+        // Act
+        double result = service.CelsiusToFahrenheit(input);
+
+        // Assert
+        Assert.Equal(32, result);
+    }
+
+    [Fact]
+    public void KilometersToMiles_ShouldConvertCorrectly()
+    {
+        // Arrange
+        var service = new ConversionService();
+        
+        // Act
+        double result = service.KilometersToMiles(1);
+        
+        // Assert
+        Assert.Equal(0.621371, result, 5); 
+    }
+
+    [Fact]
+    public void KilogramsToPounds_ShouldConvertCorrectly()
+    {
+        // Arrange
+        var service = new ConversionService();
+        
+        // Act
+        double result = service.KilogramsToPounds(1);
+        
+        // Assert
+        Assert.Equal(2.20462, result, 5);
+    }
+}
